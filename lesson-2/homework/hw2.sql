cf6a10b6440385c855903fa94bfc4200625ef3dc
Bionicuss <erkinov.bobur.97@gmail.com>
2025-07-29 15:25:42 +0500

    Update hw2.sql

--- lesson-2/homework/hw2.sql
+++ lesson-2/homework/hw2.sql
@@ -34,3 +34,67 @@ ALTER COLUMN Salary FLOAT;
     DepartmentName VARCHAR(50)
   --
 10.DELETE FROM Employees;
+--
+11.CREATE TABLE TempDepartments (
+    DepartmentID INT,
+    DepartmentName VARCHAR(50));
+INSERT INTO TempDepartments (DepartmentID, DepartmentName)
+VALUES 
+    (1, 'Management'),
+    (2, 'Sales'),
+    (3, 'IT'),
+    (4, 'Finance'),
+    (5, 'HR');
+--
+12.UPDATE Employees
+SET Department = 'Management'
+WHERE Salary > 5000;
+--
+13. TRUNCATE TABLE Employees;
+--
+14. ALTER TABLE Employees
+DROP COLUMN Department;
+--
+15.
+EXEC sp_rename 'Employees', 'StaffMembers';
+--
+16.DROP TABLE Departments;
+--
+17. CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10, 2),
+    Description VARCHAR(255)
+--
+18.ALTER TABLE Products
+ADD CONSTRAINT CHK_Price_Positive CHECK (Price > 0);
+--
+19.ALTER TABLE Products
+ADD StockQuantity INT DEFAULT 50;
+--
+20.EXEC sp_rename 'Products.Category', 'ProductCategory', 'COLUMN';
+--
+21.INSERT INTO Products (ProductID, ProductName)
+VALUES 
+    (1, 'Laptop', 'Electronics'),
+    (2, 'Office Chair', 'Furniture'),
+    (3, 'Smartphone', 'Electronics'),
+    (4, 'Notebook', 'Stationery'),
+    (5, 'Desk Lamp', 'Electronics');
+--
+22.SELECT * INTO Products_Backup
+FROM Products
+--
+23. EXEC sp_rename 'Products', 'Inventory';
+--
+24. ALTER TABLE Inventory
+ALTER COLUMN Price FLOAT;
+--
+25. CREATE TABLE Inventory_New (
+    ProductCode INT IDENTITY(1000,5) PRIMARY KEY,
+    ProductID INT,
+    ProductName VARCHAR(100),
+    ProductCategory VARCHAR(50));
+   
+
