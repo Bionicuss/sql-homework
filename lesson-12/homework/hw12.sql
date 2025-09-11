fb3ec707c28b84883c9a4c0fbdbb3f34f19334f8
Bionicuss <erkinov.bobur.97@gmail.com>
2025-09-11 11:53:41 +0500

    Update hw12.sql

--- lesson-12/homework/hw12.sql
+++ lesson-12/homework/hw12.sql
@@ -26,3 +26,25 @@ FROM Person p1
 JOIN Person p2
   ON p1.email = p2.email
  AND p1.id > p2.id;
+5.
+SELECT DISTINCT g.ParentName
+FROM girls g
+WHERE g.ParentName NOT IN (
+    SELECT b.ParentName
+    FROM boys b
+);
+7.
+SELECT 
+    ISNULL(c1.Item, '') AS [Item Cart 1],
+    ISNULL(c2.Item, '') AS [Item Cart 2]
+FROM Cart1 c1
+FULL OUTER JOIN Cart2 c2
+    ON c1.Item = c2.Item;
+8.
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+    ON c.id = o.customerId
+WHERE o.id IS NULL;
+9.
+
